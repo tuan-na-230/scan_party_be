be0a48070dd3bd13d6935cce2e762779d76263fa
tuanna3 <tuanna3@rikkeisoft.com>
2021-03-17 14:17:46 +0700

    fix bug

--- modules/event/index.js
+++ modules/event/index.js
@@ -1,31 +1,44 @@
-const eventModel = require("./model")
+const eventModel = require("./model");
 
 const eventHandle = {
-    async getManyEvent(req, res, next) {
-        if (req.body) {
-            try {
-                const item = await eventModel.find();
-                res.status(200).json(item)
-            } catch (error) {
-                next(error)
-            }
-        } else {
-            res.status(401).json({ message: 'required_body' })
-        }
-    },
-
-    async createEvent(req, res, next) {
-        if (req.body) {
-            try {
-                const item = await eventModel.create(req.body);
-                item && res.status(200).json({ message: 'create_successfully' });
-            } catch (error) {
-                next(error)
-            }
-        } else {
-            res.status(401).json({ message: 'required_body' })
-        }
+  async getManyEvent(req, res, next) {
+    if (req.body) {
+      try {
+        const item = await eventModel.find();
+        res.status(200).json(item);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      res.status(401).json({ message: "required_body" });
     }
-}
+  },
+
+  async createEvent(req, res, next) {
+    if (req.body) {
+      try {
+        const item = await eventModel.create(req.body);
+        item && res.status(200).json({ message: "create_successfully" });
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      res.status(401).json({ message: "required_body" });
+    }
+  },
+
+  async createFullEvent(req, res, next) {
+    if (req.body) {
+      try {
+        const item = await eventModel.create(req.body.evenInfo);
+        item && res.status(200).json({ message: "create_successfully" });
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      res.status(401).json({ message: "required_body" });
+    }
+  },
+};
 
 module.exports = eventHandle;
